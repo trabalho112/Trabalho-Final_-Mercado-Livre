7b4e07b42445f8b4fb9968dd353e725e430e482d
trabalho112 <trabalhofinal112@gmail.com>
2024-10-12 23:16:14 -0300

    Add files via upload

---
+++ Trabalho final/src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+#include "../include/Logar.h"
+#include "../include/Menu.h"
+#include "../include/Produtos.h"
+
+    
+
+int main(int argc, char const *argv[]){
+    MenuCadastro();
+    return sucesso;
+
+}
