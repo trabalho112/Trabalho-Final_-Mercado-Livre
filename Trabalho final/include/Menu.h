3061b80c82399eb02bbc7caa764c38b51e110b84
trabalho112 <trabalhofinal112@gmail.com>
2024-11-09 03:19:27 -0300

    Add files via upload

---
+++ Trabalho final/include/Menu.h
@@ -0,0 +1,22 @@
+#ifndef MenuH
+#define MenuH
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+#include "../include/Logar.h"     // Inclui primeiro Logar.h, que define produto_s e perfis_s
+#include "../include/Produtos.h"  // Depois Produtos.h, que depende de produto_s e perfis_s
+#include "../include/Menu.h"
+#include "../include/Imagens.h"
+
+
+produto_s* MenuLogado(perfis_s* perfis, int n_perfis, int n_logado, int n_produtos, produto_s * produtos);
+
+void MenuCadastro();
+
+#endif
